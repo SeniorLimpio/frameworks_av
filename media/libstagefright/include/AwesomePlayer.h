0d3fb68f00321c8818b667f3fcbb53aab7542857
Surajit Podder <spodder@codeaurora.org>
2014-02-20 16:22:41 +0530

    libstagefright: Render video frames on vsync
    Render video frames on vsync, which helps to
    synchronize display and decoder activity, and
    reduces power consumption
    
    CRs-Fixed: 649540
    Change-Id: I0981d7723621de963d22f7fd7ca0eed48f016090

--- media/libstagefright/include/AwesomePlayer.h
+++ media/libstagefright/include/AwesomePlayer.h
@@ -31,6 +31,8 @@
 #include <utils/threads.h>
 #include <drm/DrmManagerClient.h>
 
+#include "ExtendedUtils.h"
+
 namespace android {
 
 struct AudioPlayer;
@@ -412,6 +414,8 @@ private:
     AwesomePlayer &operator=(const AwesomePlayer &);
     bool mReadRetry;
     bool mCustomAVSync;
+
+    sp<VSyncLocker> mVSyncLocker;
 };
 
 }  // namespace android
