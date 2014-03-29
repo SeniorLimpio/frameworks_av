2f99049d96ff29e4058194a6d5c941d39d33489a
SeniorLimpio <seniorlimpio1@gmail.com>
2014-03-29 17:29:22 +1300

    Fno-strict-aliasing

--- media/libstagefright/Android.mk
+++ media/libstagefright/Android.mk
@@ -125,6 +125,8 @@ LOCAL_C_INCLUDES += \
         $(TOP)/hardware/qcom/media/mm-core/inc
 endif
 
+LOCAL_CFLAGS += -fno-strict-aliasing
+
 LOCAL_SHARED_LIBRARIES := \
         libbinder \
         libcamera_client \
