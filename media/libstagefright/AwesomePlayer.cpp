425ee75119c6ffb80ac6e2d905760bdaef8bf23c
Steve Kondik <shade@chemlab.org>
2014-10-20 00:23:46 -0700

    stagefright: Fix crash if PCM offload gets a null source
    Change-Id: I4db08355fb132288cec2ba99ec4dcb77f1618add

--- media/libstagefright/AwesomePlayer.cpp
+++ media/libstagefright/AwesomePlayer.cpp
@@ -1926,7 +1926,9 @@ status_t AwesomePlayer::initAudioDecoder() {
             mAudioSource = mAudioTrack;
 #ifndef QCOM_DIRECTTRACK
         } else {
-            mOmxSource->getFormat()->setInt32(kKeySampleBits, 16);
+            if (mOmxSource != NULL) {
+                mOmxSource->getFormat()->setInt32(kKeySampleBits, 16);
+            }
             mAudioSource = mOmxSource;
 #endif
         }
