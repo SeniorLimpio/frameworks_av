f7f44b5caf8475de56b535bb19b42cd6d901d176
Arne Coucheron <arco68@gmail.com>
2014-10-25 06:34:08 +0200

    av: Various fixes and cleanups
    Change-Id: I82223d8cd3e2b3015e6c0e1dd9ddc5ec8abab525

--- media/libmedia/AudioTrack.cpp
+++ media/libmedia/AudioTrack.cpp
@@ -419,6 +419,7 @@ status_t AudioTrack::set(
     }
 #endif
 
+#ifndef QCOM_DIRECTTRACK
     if (audio_is_linear_pcm(format)) {
         mFrameSize = channelCount * audio_bytes_per_sample(format);
         mFrameSizeAF = channelCount * sizeof(int16_t);
@@ -426,6 +427,7 @@ status_t AudioTrack::set(
         mFrameSize = sizeof(uint8_t);
         mFrameSizeAF = sizeof(uint8_t);
     }
+#endif
 
     audio_io_handle_t output = AudioSystem::getOutput(
                                     streamType,
